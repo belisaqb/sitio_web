f86abda1d880afbf462187b852411f7e11bd55a3
Isabel QB <dianaqb09@gmail.com>
2023-05-17 19:32:13 -0600

    Componente Card con Vue

---
+++ dist/js/components/RecipeCard.js
@@ -0,0 +1,46 @@
+app.component('recipe-card', {
+    props: {
+        image: {
+            type: String,
+            default: "./imgs/card.webp"
+        },
+        name: {
+            type: String,
+            default: "Pancakes"
+        },
+        total_time: {
+            type: String,
+            default: "20"
+        },
+        portions: {
+            type: String,
+            default: "6"
+        },
+        level: {
+            type: String,
+            default: "Media"
+        }
+    },
+    template:
+    /*html*/
+    `<div class="main-card text-start pb-4">
+        <div class="img-card-recipe">
+            <img :src="image" class="card-img-top rounded-top" alt="name">
+        </div>
+
+        <div class="card-body d-flex">
+            <h3 class="card-title p-3 fw-bold fst-italic">
+                {{ name }}
+            </h3>
+        </div>
+        <ul class="list-unstyled ps-4 mb-3">
+            <li class="card-text mb-2">{{ total_time }} mins</li>
+            <li class="card-text mb-2">{{ portions }} porciones.
+            </li>
+            <li class="card-text mb-2">{{ level }}</li>
+        </ul>
+        <a class="d-flex btn-dark justify-content-center mx-3" href="recipe.html">
+            <div class="fw-bold px-5">Ver más</div>
+        </a>
+    </div>`
+})
